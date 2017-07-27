b37d1c8c472261717ddc1bc18681f81da1a712bd
fizz <864759073@qq.com>
2017-07-27 10:48:14 +0800

    去掉traits

--- src/DB.php
+++ src/DB.php
@@ -4,7 +4,32 @@ namespace Fizzday\FizzDB;
 
 class DB
 {
-    protected $builder = '\Fizzday\Database\DBBuilder';
+//    protected $builder = '\Fizzday\Database\DBBuilder';
 
-    use \Fizzday\FizzTraits\CallTrait;
+//    use \Fizzday\FizzTraits\CallTrait;
+    public static $instance;
+
+    /**
+     * @param $method
+     * @param $params
+     * @return mixed
+     */
+    public function __call( $method, $params ) {
+        if ( !( self::$instance ) ) {
+            $class_name = empty(self::$service) ? get_called_class() . 'Builder' : self::$service;
+            self::$instance = new $class_name();
+        }
+
+        return call_user_func_array( [ self::$instance, $method ], $params );
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
+    public static function __callStatic($method, $params)
+    {
+        return call_user_func_array([new static(), $method], $params);
+    }
 }
